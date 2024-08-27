83acb2e45f58e59b6898fcd7d447cd57278d184b
David Jimenez <arcanio457@gmail.com>
2024-08-27 15:04:12 -0400

    Creada la lógica del humano

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ const getComputerChoice = () => {
     }
 }
 
+const getHumanChoice = () => {
+    let choice = Number(prompt("Enter a number: 1. Scissors, 2. Paper, 3.Rock", ""));
+
+    switch (choice) {
+        case 1:
+            console.log("Scissors");
+            break;
+        case 2:
+            console.log("Paper");
+            break;
+        case 3:
+            console.log("Rock");
+            break;
+        default:
+            alert("Please choice a correct number");
+    }
+}
+
+getHumanChoice();
 getComputerChoice();
