cee6a5471d8658aa5e8c52e14ee2cecacb674e2f
David Jimenez <arcanio457@gmail.com>
2024-08-27 15:47:14 -0400

    Creada la lógica del juego

--- script.js
+++ script.js
@@ -4,33 +4,87 @@ const random = (max) => {
 
 const getComputerChoice = () => {
     let num = random(3);
+    let computerChoice = "";
 
     if (num === 0) {
-        return console.log("Scissors");
+        return computerChoice = "Scissors";
     } else if (num === 1) {
-        return console.log("Paper");
+        return computerChoice = "Paper";
     } else {
-        return console.log("Rock");
+        return computerChoice = "Rock";
     }
 }
 
 const getHumanChoice = () => {
     let choice = Number(prompt("Enter a number: 1. Scissors, 2. Paper, 3.Rock", ""));
+    let humanChoice = "";
 
-    switch (choice) {
-        case 1:
-            console.log("Scissors");
-            break;
-        case 2:
-            console.log("Paper");
-            break;
-        case 3:
-            console.log("Rock");
-            break;
-        default:
-            alert("Please choice a correct number");
+    if (choice === 1) {
+        return humanChoice = "Scissors";
+    } else if (choice === 2) {
+        return humanChoice = "Paper";
+    } else if (choice === 3) {
+        return humanChoice = "Rock";
+    } else {
+        alert("Please choice a correct number");
     }
 }
 
-getHumanChoice();
-getComputerChoice();
+const playRound = (humanChoice, computerChoice) => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if (humanChoice === "Scissors" && computerChoice === "Scissors") {
+        console.log("It's a draw");
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        console.log("It's a draw");
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Rock" && computerChoice === "Rock") {
+        console.log("It's a draw");
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("The computer won, Rock rules");
+        computerScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You won, Scissors rules");
+        humanScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You won, Rock rules");
+        humanScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log("The computer won, Paper rules");
+        computerScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You won, Paper rules");
+        humanScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("The computer won, Scissors rules");
+        computerScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+    } else {
+        alert("Error");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
